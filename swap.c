1afa289bc6b9621193f06325ed10c97b927b050c
Ivan Budko <ivanbudko@MacBook-Air-Ivan.local>
2025-06-28 14:31:48 +0200

    swap operations done

---
+++ swap.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ivanbudko <ivanbudko@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/28 14:10:52 by ivanbudko         #+#    #+#             */
+/*   Updated: 2025/06/28 14:30:57 by ivanbudko        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	swap(t_node **stack)
+{
+	t_node	*first;
+	t_node	*second;
+	
+	if (!stack || !*stack || !(*stack)->next)
+		return ;
+	first = *stack;
+	second = first->next;
+	first->next = second->next;
+	second->next = first;
+	*stack = second;
+}
+
+void	sa(t_node **a)
+{
+	swap(a);
+	ft_printf("sa\n");
+}
+
+void	sb(t_node **b)
+{
+	swap(b);
+	ft_printf("sb\n");
+}
+
+void	ss(t_node **a, t_node **b)
+{
+	swap(a);
+	swap(b);
+	ft_printf("ss\n");
+}
